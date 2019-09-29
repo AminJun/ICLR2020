1b3cfb0a60649945fea248fa4e69fe55a154a8ec
AminJun <mohammad.amin.ghiasi@gmail.com>
2019-09-29 14:28:00 -0400

    Add progressbar

--- findall.sh
+++ findall.sh
@@ -1,9 +1,10 @@
 ct=0
+mx=$(ls | grep "pdf" | wc -l)
 for i in `ls . | grep pdf` ; do 
 	count=$(pdfgrep -i ".*$1.*" $i | wc -l) 
 	if [ "${count}" -gt 0 ] ; then 
 		echo $i 
 	fi
 	ct=$((ct+1))
-	>&2 echo $ct
+	>&2 echo -en "\r${ct}/${mx}"
 done
